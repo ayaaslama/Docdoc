2a4e0d1e55156c61273fb0b39ee94bbe3d440830
ayaaslama <ayamhmd089@gmail.com>
2024-09-03 20:17:55 +0300

    - added images & svgs folders for assets - added flutter_svg & added flutter_screenutil - make a file for doc app client - added Navigation extension - added routing folder that contains router class and routes file - added colors & text styles - added empty login screen - added onboarding screen

--- lib/features/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:doctor/features/doc_app.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const DocApp());
-}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:doctor/core/routing/app_router.dart';
+import 'package:doctor/doc_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(DocApp(
+    appRouter: AppRouter(),
+  ));
+}
